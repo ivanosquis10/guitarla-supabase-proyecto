3e46d2ffe4660f802f660d60d682a846621e9539
ivanosquis10 <83567373+ivanosquis10@users.noreply.github.com>
2023-03-07 18:07:46 -0400

    se agrego la notificacion al eliminar y las variables de entorno

--- pages/carrito.js
+++ pages/carrito.js
@@ -33,6 +33,18 @@ export default function Carrito({
     })
   }
 
+  function notificacionEliminar() {
+    toast.error('Eliminado del carrito😔', {
+      position: 'top-right',
+      id: 'clipboard',
+      style: {
+        borderRadius: '10px',
+        background: '#0c4a6e',
+        color: '#fff',
+      },
+    })
+  }
+
   return (
     <Layout title="Carrito compras">
       <main className="container mx-auto my-2">
@@ -46,85 +58,90 @@ export default function Carrito({
               Articulos
             </h2>
 
-            {carrito.length === 0
-              ? <p className="uppercase text-center font-bold">carrito vacio</p>
-              : carrito.map((producto) => (
-                  <section key={producto.id}>
-                    <div className="bg-slate-600/80 w-full md:grid md:grid-cols-3 text-slate-900 rounded-md">
-                      <div className="flex justify-center ">
-                        <Image
-                          className="rounded-l-md"
-                          src={producto.image}
-                          width={250}
-                          height={150}
-                          alt={producto.nombre}
-                          priority
-                        />
-                      </div>
+            {carrito.length === 0 ? (
+              <p className="uppercase text-center font-bold">carrito vacio</p>
+            ) : (
+              carrito.map((producto) => (
+                <section key={producto.id}>
+                  <div className="bg-slate-600/80 w-full md:grid md:grid-cols-3 text-slate-900 rounded-md">
+                    <div className="flex justify-center ">
+                      <Image
+                        className="rounded-l-md"
+                        src={producto.image}
+                        width={250}
+                        height={150}
+                        alt={producto.nombre}
+                        priority
+                      />
+                    </div>
 
-                      <div className="col-span-2 flex flex-col gap-5 p-5">
-                        <h3 className="uppercase text-3xl font-bold text-slate-100">
-                          {producto.nombre}
-                        </h3>
-                        <div className="">
-                          <div className="flex items-center justify-between">
-                            <p className="text-2xl font-bold text-slate-200">
-                              Cantidad:{' '}
-                            </p>
+                    <div className="col-span-2 flex flex-col gap-5 p-5">
+                      <h3 className="uppercase text-3xl font-bold text-slate-100">
+                        {producto.nombre}
+                      </h3>
+                      <div className="">
+                        <div className="flex items-center justify-between">
+                          <p className="text-2xl font-bold text-slate-200">
+                            Cantidad:{' '}
+                          </p>
 
-                            <select
-                              className="bg-slate-700 py-2 px-8 rounded-md text-white text-center text-lg"
-                              onChange={(e) =>
-                                actualizarCantidad({
-                                  id: producto.id,
-                                  cantidad: e.target.value,
-                                })
-                              }
-                              value={producto.cantidad}
-                            >
-                              <option className="bg-slate-600" value="1">
-                                1
-                              </option>
-                              <option className="bg-slate-600" value="2">
-                                2
-                              </option>
-                              <option className="bg-slate-600" value="3">
-                                3
-                              </option>
-                              <option className="bg-slate-600" value="4">
-                                4
-                              </option>
-                              <option className="bg-slate-600" value="5">
-                                5
-                              </option>
-                            </select>
-                          </div>
+                          <select
+                            className="bg-slate-700 py-2 px-8 rounded-md text-white text-center text-lg"
+                            onChange={(e) =>
+                              actualizarCantidad({
+                                id: producto.id,
+                                cantidad: e.target.value,
+                              })
+                            }
+                            value={producto.cantidad}
+                          >
+                            <option className="bg-slate-600" value="1">
+                              1
+                            </option>
+                            <option className="bg-slate-600" value="2">
+                              2
+                            </option>
+                            <option className="bg-slate-600" value="3">
+                              3
+                            </option>
+                            <option className="bg-slate-600" value="4">
+                              4
+                            </option>
+                            <option className="bg-slate-600" value="5">
+                              5
+                            </option>
+                          </select>
+                        </div>
 
-                          <div className="mt-2 ">
-                            <p className="text-2xl text-slate-100 mb-2 bg-slate-600/80 rounded-md py-1">
-                              Precio:{' '}
-                              <span className="font-bold text-slate-300">
-                                ${producto.precio}
-                              </span>{' '}
-                            </p>
-                            <p className="text-2xl text-slate-100 bg-slate-600/80 rounded-md py-1">
-                              Subtotal:{' '}
-                              <span className="font-bold text-slate-300">
-                                ${producto.cantidad * producto.precio}
-                              </span>{' '}
-                            </p>
-                            <button
-                              className="uppercase text-slate-100 font-bold bg-slate-700 w-full p-2 rounded-md mt-5 ease-in-out duration-300 hover:bg-slate-800"
-                              onClick={() => eliminarProducto(producto.id)}
-                            >
-                              Borrar producto
-                            </button>
-                          </div>
+                        <div className="mt-2 ">
+                          <p className="text-2xl text-slate-100 mb-2 bg-slate-600/80 rounded-md py-1">
+                            Precio:{' '}
+                            <span className="font-bold text-slate-300">
+                              ${producto.precio}
+                            </span>{' '}
+                          </p>
+                          <p className="text-2xl text-slate-100 bg-slate-600/80 rounded-md py-1">
+                            Subtotal:{' '}
+                            <span className="font-bold text-slate-300">
+                              ${producto.cantidad * producto.precio}
+                            </span>{' '}
+                          </p>
+                          <button
+                            className="uppercase text-slate-100 font-bold bg-slate-700 w-full p-2 rounded-md mt-5 ease-in-out duration-300 hover:bg-slate-800"
+                            onClick={() => {
+                              notificacionEliminar()
+                              eliminarProducto(producto.id)
+                            }}
+                          >
+                            Borrar producto
+                          </button>
                         </div>
                       </div>
                     </div>
-                  </section>
-                ))}
+                  </div>
+                </section>
+              ))
+            )}
           </div>
 
           <aside className="w-full h-fit sticky top-2">
--- supabase/supabase.js
+++ supabase/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  'https://msetvyvvrzdkntctbebj.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zZXR2eXZ2cnpka250Y3RiZWJqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzczNTg4NjMsImV4cCI6MTk5MjkzNDg2M30.4WroVyP-I_423sieI59c6PhpNC7oAYymTfo7xicVbRw'
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 )
